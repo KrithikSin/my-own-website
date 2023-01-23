71e7bd3c04b430156ca29dddccab25acc1584c48
Krithik Singh <krithiksingh@gmail.com>
2023-01-23 11:19:01 -0500

    restarted website from scratch

--- script.js
+++
@@ -1,7 +0,0 @@
-
-<img id="right-image" src="https://media.tenor.com/G7VxP0dvvZQAAAAC/sasuke-angry.gif" alt="young sasuke gif"></img>
-
-const gif = document.querySelector("#right-image");
-gif.addEventListener("click", function() {
-    window.location.href = "https://www.youtube.com/watch?v=pb-6C9D05AM&ab_channel=Zekkson";
-});
